81788fcf98a94bd3bd98d4da2fc20fe48d207504
blacknoobyyy <90363111+blacknooby@users.noreply.github.com>
2026-03-08 17:32:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-
 function login(){
 
-let user = document.getElementById("username").value;
-let pass = document.getElementById("password").value;
+let user=document.getElementById("username").value;
 
-if(user === "nhom2" && pass === "nhom2"){
+let pass=document.getElementById("password").value;
 
-window.location.href="dashboard.html";
+if(user==="nhom2" && pass==="nhom2"){
 
-}else{
+window.location="dashboard.html";
 
-document.getElementById("error").innerText="Sai tài khoản hoặc mật khẩu";
+}
+
+else{
+
+document.getElementById("error").innerText="Sai tài khoản";
 
 }
 
@@ -19,29 +21,106 @@ document.getElementById("error").innerText="Sai tài khoản hoặc mật khẩu
 
 function logout(){
 
-window.location.href="index.html";
+window.location="index.html";
 
 }
 
 
-/* Fake sensor data */
 
-function randomData(){
+/* SENSOR SIMULATION */
 
-let temp = (20 + Math.random()*10).toFixed(1);
-let hum = (60 + Math.random()*20).toFixed(1);
-let rain = (Math.random()*5).toFixed(1);
-let wind = (Math.random()*8).toFixed(1);
+let chart;
+
+function initChart(){
+
+const ctx=document.getElementById("tempChart").getContext("2d");
+
+chart=new Chart(ctx,{
+
+type:"line",
+
+data:{
+
+labels:[],
+
+datasets:[{
+
+label:"Temperature",
+
+data:[],
+
+borderColor:"cyan",
+
+fill:false
+
+}]
+
+},
+
+options:{
+
+scales:{y:{beginAtZero:false}}
+
+}
+
+});
+
+}
+
+
+
+function updateSensor(){
+
+let temp=(20+Math.random()*10).toFixed(1);
+
+let hum=(60+Math.random()*20).toFixed(1);
+
+let rain=(Math.random()*5).toFixed(1);
+
+let wind=(Math.random()*8).toFixed(1);
+
 
 if(document.getElementById("temp")){
 
 document.getElementById("temp").innerText=temp+" °C";
+
 document.getElementById("humidity").innerText=hum+" %";
+
 document.getElementById("rain").innerText=rain+" mm";
+
 document.getElementById("wind").innerText=wind+" m/s";
 
-}
+
+let time=new Date().toLocaleTimeString();
+
+chart.data.labels.push(time);
+
+chart.data.datasets[0].data.push(temp);
+
+if(chart.data.labels.length>10){
+
+chart.data.labels.shift();
+
+chart.data.datasets[0].data.shift();
 
 }
 
-setInterval(randomData,2000);
+chart.update();
+
+}
+
+}
+
+
+
+window.onload=function(){
+
+if(document.getElementById("tempChart")){
+
+initChart();
+
+setInterval(updateSensor,2000);
+
+}
+
+};
