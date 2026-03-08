2766d46054a958d96df759743b9e5978b8882f96
blacknoobyyy <90363111+blacknooby@users.noreply.github.com>
2026-03-08 19:33:16 +0700

    Update map.js

--- map.js
+++ map.js
@@ -1,10 +1,20 @@
-var map = L.map('map').setView([10.98,106.65], 8);
+var map = L.map('map').setView([10.98, 106.65], 8);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-maxZoom:19
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
 }).addTo(map);
 
+// Tạo icon tùy chỉnh bằng HTML & CSS thay vì dùng ảnh tĩnh
+var weatherIcon = L.divIcon({
+    className: 'custom-marker', // Class CSS để style
+    html: '<div class="marker-content">📡</div>', // Bạn có thể đổi 📡 thành 🌤️ hoặc 📍 tùy thích
+    iconSize: [44, 44],
+    iconAnchor: [22, 44], // Điểm neo ở giữa phía dưới cùng của icon
+    popupAnchor: [0, -44] // Popup sẽ mở ra ở phía trên icon
+});
 
-var marker = L.marker([10.98,106.65]).addTo(map);
+// Thêm marker vào bản đồ với icon mới
+var marker = L.marker([10.98, 106.65], { icon: weatherIcon }).addTo(map);
 
-marker.bindPopup("Weather Station").openPopup();
+// Chút điệu đà cho nội dung popup
+marker.bindPopup("<b>IoT Weather Station</b><br>Trạm Nhóm 2 đang hoạt động!").openPopup();
